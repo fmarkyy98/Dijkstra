c8388f3d415a6e03b4c9fd96408bbb89a39d3b02
Suli <Suli@192.168.1.211>
2018-02-09 08:14:38 +0100

    DijkstraAlgorithm.java Finished.

--- Dijkstra/src/DijkstraAlgorithm.java
+++ Dijkstra/src/DijkstraAlgorithm.java
@@ -1,9 +1,11 @@
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class DijkstraAlgorithm {
 
-    public String dijkstra(String to, String from) {
+    public LinkedList<String> dijkstra(String to, String from) {
+        LinkedList<String> result = new LinkedList<String>();
         Node n;
         n = NodeFactory.getExistingNode(from);
         n.dist = 0;
@@ -12,8 +14,20 @@ public class DijkstraAlgorithm {
             NodeFactory.removeNode(u);
             if (to.equals(u.name)) {
                 Node current = u;
+                while (current.prev != null) {
+                    result.add(current.name);
+                    current = current.prev;
+                }
+                return result;
+            }
+            for (Pair v : u.neigs) {
+                int a = u.dist + v.cost;
+                if (a < v.node.dist) {
+                    v.node.dist = a;
+                    v.node.prev = u;
+                }
             }
         }
-        return null;
+        return new LinkedList();
     }
 }
